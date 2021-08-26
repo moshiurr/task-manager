73c6f962ca6db90ae70ef6cd25e9b8c97ef53f26
Moshiur Rahman <mrahman9@upei.ca>
2021-08-26 15:22:37 -0300

    first commit

---
+++ mongodb.js
@@ -0,0 +1,64 @@
+const { MongoClient, ObjectId } = require('mongodb')
+
+const uri = "mongodb+srv://virion:virion@cluster0.ulsrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const dbName = "udemy-task";
+
+MongoClient.connect(uri, { useNewUrlParser: true}, (error,client) =>{
+    if(error) return console.error(error);
+
+    console.log("Connection successful!");
+
+    const db = client.db(dbName);
+
+    const collection = db.collection('users');
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Complete two  setup in SU',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Fix connection error in MongoDB',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Work at 5pm',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if(error) return console.log("Unable to insert")
+    
+    //     console.log(result);
+    // })
+
+
+    // db.collection('users').find({age:12}).toArray((error, user)=>{
+    //     if(error) return console.log("unable to fetch");
+
+    //     console.log(user);
+    // })
+
+
+    // db.collection('tasks').find({completed: true}).toArray((error, task)=>{ console.log(task)});
+
+    // collection.updateOne({
+    //     _id: new ObjectId("61265bad37cf00561948a3e3")
+    // }, {
+    //     $inc: {
+    //         age: 2
+    //     }
+    // }).then((result)=>{
+    //     console.log(result);
+    // }).catch(error=>{
+    //     console.log(error);
+    // })
+
+    db.collection('tasks').updateMany({},{
+        $set: {
+            completed: true
+        }
+    }).then((result)=>{console.log(result)}).catch(error=>{console.log(error)})
+
+})
+
---
+++ src/db/mongoose.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const uri = "mongodb+srv://virion:virion@cluster0.ulsrf.mongodb.net/task-manager-api?retryWrites=true&w=majority";
+
+
+mongoose.connect(uri, {
+    useNewUrlParser: true
+})
+
+const User = mongoose.model('User',{
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+
+        //email validation using Validator package
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    age: {
+        type: Number,
+
+        //number validation using built-in validators
+        validate(value){
+            if(value < 0) throw new Error('Age must be a positive number')
+        }
+    }
+})
+
+
+const me = new User({
+    name: "moshiur",
+    email: "abc@d.ca",
+    age: 23
+})
+
+me.save().then(()=>{
+    console.log(me)
+}).catch((err)=>{
+    console.log(err);
+})
+
+const Task = mongoose.model('Task',{
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+// const t = new Task({
+//     description: 'Complete Mongo Portion',
+//     completed: true
+// })
+
+
+// t.save().then(()=>{
+//     console.log(t);
+// }).catch((err)=>{
+//     console.log(err);
+//})

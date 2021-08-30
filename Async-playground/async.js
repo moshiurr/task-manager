996249d14e1f3b9b487feec6cebf5457b7befcbd
Moshiur Rahman <mrahman9@upei.ca>
2021-08-30 10:39:40 -0300

    async-await challenge done

--- Async-playground/async.js
+++ Async-playground/async.js
@@ -1,6 +1,10 @@
 const add = (a, b) => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
+            //just to check what happend when a await function get rejected in async function in doWork method
+            if(a<0 || b<0){
+                return reject("Numbers must be non negative")
+            }
 			resolve(a + b);
 		}, 2000);
 	});
@@ -10,7 +14,10 @@ const add = (a, b) => {
 const doWork = async () =>{
     
     const sum = await add(1,99)
-    return sum;
+    const sum2 = await add(sum,50)
+    const sum3 = await add(sum2, -3)
+    //if anything gets rejected, the catch of doWork chaining will catch the error and will stop the code executuion
+    return sum3;
 
 }
 
--- playground/promise-chain-challenge.js
+++ playground/promise-chain-challenge.js
@@ -4,14 +4,31 @@ const Task = require("../src/models/task");
 
 // id to delete 6129b73b06a256cd1cc0ed58
 
-Task.findByIdAndDelete("6129b73b06a256cd1cc0ed58")
-	.then(res => {
-		console.log(res);
-		return Task.countDocuments({ completed: false });
-	})
-	.then(value => {
-		console.log(value);
-	})
-	.catch(err => {
-		console.log(err);
-	});
+// Task.findByIdAndDelete("6129b73b06a256cd1cc0ed58")
+// 	.then(res => {
+// 		console.log(res);
+// 		return Task.countDocuments({ completed: false });
+// 	})
+// 	.then(value => {
+// 		console.log(value);
+// 	})
+// 	.catch(err => {
+// 		console.log(err);
+// 	});
+
+//async-await challenge
+
+const deleteTaskAndCount = async (id) => {
+	const task = await Task.findByIdAndDelete(id);
+	const count = await Task.countDocuments({completed: false});
+	return count;
+}
+
+// new ID: 612af97d46315d24c9114b21
+
+
+deleteTaskAndCount("612af97d46315d24c9114b21").then((result) => {
+	console.log(result);
+}).catch(error=>{
+	console.log(error);
+});

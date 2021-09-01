59ac0becc60685bff97c57a686a53e1a73d5edb9
Moshiur Rahman <mrahman9@upei.ca>
2021-09-01 15:56:46 -0300

    password are stored in hash format using bcrypt

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 require("./db/mongoose");
 //above line ensures that mongoose.js file runs and we get connection to the database
 
-const User = require("./models/user");
-const Task = require("./models/task");
 
 const app = express();
 
@@ -35,3 +33,25 @@ app.use(taskRouter);
 app.listen(port, () => {
 	console.log("Server is up and running on port " + port);
 });
+
+
+/////////////////////////////////
+/// Testing bcryptJS/////////////
+
+const bcrypt = require('bcryptjs')
+
+const myFunc = async () =>{
+	const password = "ABc1234!";
+
+	const hashedPassword = await bcrypt.hash(password, 8);
+
+	console.log(password);
+	console.log(hashedPassword);
+
+	const isMatch = await bcrypt.compare("ABC1234!", hashedPassword);
+	console.log(isMatch)
+}
+
+myFunc();
+
+
--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -73,7 +73,18 @@ router.patch('/users/:id', async (req, res) => {
 	if(!isValidOperation) return res.status(400).send({error: 'Invalid updates'})
 
 	try{
-		const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+
+		const user = await User.findById(req.params.id);
+
+		updates.forEach((update) => {
+			user[update] = req.body[update]
+		});
+
+		await user.save();
+
+		//below commented code is not in use 
+
+		//const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
 		// the third param {new: true} is for getting the updated new user, not the user it find first
 		// runValidators make sure every property is valid
 

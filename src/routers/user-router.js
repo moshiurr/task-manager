c27daf7ef5bfe4e29d1100eba98de1555f245d7c
Moshiur Rahman <mrahman9@upei.ca>
2021-09-01 10:00:55 -0300

    routes moved to separate files and conntected to index.js

---
+++ src/routers/user-router.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const router = new express.Router()
+
+const User = require("../models/user");
+
+/////////////////////////////////
+//all routes releated to USERS//
+///////////////////////////////
+
+router.post("/users", async (req, res) => {
+	//connecting to the User Schema
+	const user = new User(req.body);
+
+	//using Aysnc-Await ps: make sure to make the function 'async'
+	try{
+		await user.save();
+		res.status(201).send(user);
+	}catch(err){
+		res.status(400).send(err);
+	}
+
+	//Using promise chainnig
+
+	// user
+	// 	.save()
+	// 	.then(() => {
+	// 		res.status(201).send(user);
+	// 	})
+	// 	.catch(e => {
+	// 		res.status(400).send(e.message);
+	// 	});
+});
+
+//get all user route
+
+router.get("/users", async (req, res) => {
+
+	//Using Async-Await 
+	try {
+		const users = await User.find({});
+		res.status(200).send(users);
+	}catch(err) {
+		res.status(400).send(err);
+	}
+});
+
+//get individual user route
+// :id is the dynamic variable provided by express
+
+router.get("/users/:id", async (req, res) => {
+	const _id = req.params.id;
+
+	//Using Async-Await
+
+	try{
+		const user = await User.findById(_id);
+		if(!user) return res.status(404).send();
+		res.send(user);
+	}catch(err) {
+		res.status(500).send(err);
+	}
+});
+
+//update route for user
+
+router.patch('/users/:id', async (req, res) => {
+
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['name', 'email', 'password', 'age'];
+
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+	if(!isValidOperation) return res.status(400).send({error: 'Invalid updates'})
+
+	try{
+		const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+		// the third param {new: true} is for getting the updated new user, not the user it find first
+		// runValidators make sure every property is valid
+
+		if(!user) return res.status(404).send();
+
+		res.send(user);
+	}catch(e){
+		res.status(500).send(e);
+	}
+})
+
+//delete user route
+
+router.delete('/users/:id', async (req, res) => {
+	
+	try{
+		const user = await User.findByIdAndDelete(req.params.id);
+
+		if(!user) return res.status(404).send();
+
+		res.send(user);
+
+	}catch(e){
+		res.status(400).send(e);
+	}
+})
+
+module.exports = router;

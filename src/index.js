a54f7159085f83a1602770c21695239bdc74f69c
Moshiur Rahman <mrahman9@upei.ca>
2021-09-01 23:34:28 -0300

    jwt checked

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 require("./db/mongoose");
 //above line ensures that mongoose.js file runs and we get connection to the database
 
-
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -12,12 +11,11 @@ const port = process.env.PORT || 3000;
 //automatically convert express requests to JSON objects
 app.use(express.json());
 
-
 ///////////////////////////////////////////////////////
 ////attaching the user router from another file////////
 ///////////////////////////////////////////////////////
 
-const userRouter = require('./routers/user-router')
+const userRouter = require("./routers/user-router");
 
 app.use(userRouter);
 
@@ -25,33 +23,28 @@ app.use(userRouter);
 ////attaching the task router from another file////////
 ///////////////////////////////////////////////////////
 
-const taskRouter = require('./routers/task-router')
+const taskRouter = require("./routers/task-router");
 
 app.use(taskRouter);
-
 
 app.listen(port, () => {
 	console.log("Server is up and running on port " + port);
 });
 
+/////////////////////////////////
+/// Testing bcryptJS/////////////
 
-// /////////////////////////////////
-// /// Testing bcryptJS/////////////
+const jwt = require("jsonwebtoken");
 
-// const bcrypt = require('bcryptjs')
+const myFunc = async () => {
+	const secret = "abcdefghi";
+	const token = jwt.sign({ _id: "abcd123" }, secret, {
+		expiresIn: "1800 seconds",
+	});
+	console.log(token);
 
-// const myFunc = async () =>{
-// 	const password = "ABc1234!";
+	const data = jwt.verify(token, secret);
+	console.log(data);
+};
 
-// 	const hashedPassword = await bcrypt.hash(password, 8);
-
-// 	console.log(password);
-// 	console.log(hashedPassword);
-
-// 	const isMatch = await bcrypt.compare("ABC1234!", hashedPassword);
-// 	console.log(isMatch)
-// }
-
-// myFunc();
-
-
+myFunc();
